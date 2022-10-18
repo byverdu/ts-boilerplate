6eaff91cfadbf289fbee5fee88cd27705602e32f
byverdu <byverdu@gmail.com>
2022-10-18 11:53:18 +0100

    renamed folder to libray-example

--- node-example/__tests__/index.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('Name of the group', () => {
-  it('should be defined', () => {
-    expect(true).toBe(true);
-  });
-});
--- node-example/src/utils/logger.ts
+++
@@ -1,7 +0,0 @@
-type Logger = 'error' | 'warn' | 'log';
-
-const logger = (loggerType: Logger, msg: string) => {
-  console[loggerType](msg);
-};
-
-export { logger };
