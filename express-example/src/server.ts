c45f92551b2124a0c545e92a8880454527f168ee
Albert Vallverdu <byverdu@gmail.com>
2022-10-25 08:55:25 +0100

    wip express example

--- express-example/src/server.ts
+++ express-example/src/server.ts
@@ -1,6 +1,21 @@
 import express, { Express, Response } from 'express';
+import morgan from 'morgan'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+import { healthRouter } from '@routes/health'
 
 const app: Express = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.use('/health', healthRouter);
+
+app.use((req, res) => {
+    res.status(404).send(`No handler found for ${req.url}`)
+})
 
 app.get('/', (_, res: Response) => {
     res.send('Express + TypeScript Server');
